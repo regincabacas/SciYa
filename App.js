005abbcfe7bb875d0ddc222c1c3c7043950de308
rcabacas <rcabacas@gmail.com>
2018-07-10 12:38:18 +0900

    From PC1

--- App.js
+++ App.js
@@ -27,12 +27,6 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
       </View>
     );
   }
